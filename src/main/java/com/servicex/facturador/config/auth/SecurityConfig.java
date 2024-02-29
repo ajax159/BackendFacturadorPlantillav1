92604d66f896a21e00dfa1183298f67057c3884b
ajax159 <gmod159@gmail.com>
2024-02-29 13:09:56 -0500

    Update dependencies and security configuration

--- src/main/java/com/servicex/facturador/config/auth/SecurityConfig.java
+++ src/main/java/com/servicex/facturador/config/auth/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.servicex.facturador.config.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,9 +16,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.servicex.facturador.controllers._m_facturador.authController.JwtAuthenticationResponse;
 import com.servicex.facturador.controllers._m_facturador.authController.UserInfoUserDetailsService;
@@ -25,10 +33,8 @@ import com.servicex.facturador.controllers._m_facturador.authController.UserInfo
 @Deprecated
 public class SecurityConfig {
 
-
     @Autowired
     private JwtAuthenticationResponse jwtAuthenticationResponse;
-
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -37,18 +43,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Arrays.asList("*"));
+            config.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE"));
+            config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+            config.setExposedHeaders(Arrays.asList("_filename", "header-a", "_codigo"));
+            return config;
+        });
         http.csrf().disable()
-        .authorizeRequests(authorizeRequests ->
-            authorizeRequests
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/**")).permitAll()
-                .anyRequest().authenticated()
-        )
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider())
-        .addFilterBefore(jwtAuthenticationResponse, UsernamePasswordAuthenticationFilter.class);
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/**")).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationResponse, UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
+        return http.build();
     }
 
     @Bean
@@ -62,11 +74,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+
+    // @Override
+    // public void init(HttpSecurity builder) throws Exception {
+    // throw new UnsupportedOperationException("Unimplemented method 'init'");
+    // }
 
 }
